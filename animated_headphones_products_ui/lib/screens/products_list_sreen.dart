14c4c1cf84a0bc3549a11bbb69c92be459e0ad2b
MudasirHussain72 <mudasir7t1@gmail.com>
2023-10-24 04:08:51 -0700

    Adding animation in product list

--- animated_headphones_products_ui/lib/screens/products_list_sreen.dart
+++ animated_headphones_products_ui/lib/screens/products_list_sreen.dart
@@ -2,13 +2,70 @@ import 'package:animated_headphones_products_ui/models/headphones_model.dart';
 import 'package:flutter/material.dart';
 
 class ProductsListScreen extends StatefulWidget {
+  // ignore: use_key_in_widget_constructors
   const ProductsListScreen({Key? key});
 
   @override
   State<ProductsListScreen> createState() => _ProductsListScreenState();
 }
 
-class _ProductsListScreenState extends State<ProductsListScreen> {
+class _ProductsListScreenState extends State<ProductsListScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _leftSlideAnimation;
+  late Animation<Offset> _rightSlideAnimation;
+  late Animation<Offset> _containerAnimation;
+  late Animation<Offset> _sizedBoxAnimation;
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _leftSlideAnimation = Tween<Offset>(
+      begin: const Offset(-1.0, 0.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+
+    _rightSlideAnimation = Tween<Offset>(
+      begin: const Offset(1.0, 0.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+
+    _containerAnimation = Tween<Offset>(
+      begin: const Offset(-1.0, 0.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+
+    _sizedBoxAnimation = Tween<Offset>(
+      begin: const Offset(1.0, 0.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,24 +86,30 @@ class _ProductsListScreenState extends State<ProductsListScreen> {
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
-                  child: const Text(
-                    'Wireless\nHeadphones',
-                    style: TextStyle(
-                      fontSize: 24, // Adjust font size as needed
-                      fontWeight: FontWeight.bold,
+                SlideTransition(
+                  position: _containerAnimation,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 40),
+                    child: const Text(
+                      'Wireless\nHeadphones',
+                      style: TextStyle(
+                        fontSize: 24, // Adjust font size as needed
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
-                SizedBox(
-                  height: 10,
-                  width: 80,
-                  child: RotationTransition(
-                    turns: const AlwaysStoppedAnimation(270 / 360),
-                    child: Image.asset(
-                        fit: BoxFit.cover, 'assets/images/bose logo.png'),
+                SlideTransition(
+                  position: _sizedBoxAnimation,
+                  child: SizedBox(
+                    height: 10,
+                    width: 80,
+                    child: RotationTransition(
+                      turns: const AlwaysStoppedAnimation(270 / 360),
+                      child: Image.asset(
+                          fit: BoxFit.cover, 'assets/images/bose logo.png'),
+                    ),
                   ),
                 ),
               ],
@@ -61,49 +124,54 @@ class _ProductsListScreenState extends State<ProductsListScreen> {
               physics: const NeverScrollableScrollPhysics(),
               itemCount: headphones.length,
               itemBuilder: (context, index) {
-                return Container(
-                  margin: const EdgeInsets.all(10), // Consistent margin
-                  padding: const EdgeInsets.all(20), // Consistent padding
-                  width: 323,
-                  height: 495,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(34)),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: double.infinity,
-                        height: 135,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            fit: BoxFit.fitHeight,
-                            image: AssetImage(headphones[index].image),
+                return SlideTransition(
+                  position: index % 2 == 0
+                      ? _leftSlideAnimation
+                      : _rightSlideAnimation,
+                  child: Container(
+                    margin: const EdgeInsets.all(10), // Consistent margin
+                    padding: const EdgeInsets.all(20), // Consistent padding
+                    width: 323,
+                    height: 495,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(34)),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          height: 135,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              fit: BoxFit.fitHeight,
+                              image: AssetImage(headphones[index].image),
+                            ),
                           ),
                         ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            headphones[index].name,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              headphones[index].name,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            headphones[index].price,
-                            style: const TextStyle(
-                              color: Color(
-                                  0xFFEBEBEB), // Adjust text color as needed
+                            const SizedBox(height: 10),
+                            Text(
+                              headphones[index].price,
+                              style: const TextStyle(
+                                color: Color(
+                                    0xFFEBEBEB), // Adjust text color as needed
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
